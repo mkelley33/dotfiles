61ab01e6cee2e7d107af2122413e71608a5338d8
Michaux Kelley <michauxkelley@gmail.com>
2012-07-15 00:38:42 -0400

    Update instructions for setting variable that controls global git e-mail.

--- global_git.sh
+++ global_git.sh
@@ -5,6 +5,7 @@
 if [ $GIT_EMAIL = '' ]; then
   echo "Please set a global e-mail address for git."
   echo "Type something like: export GIT_EMAIL='test@example.com'"
+  echo "Next re-run this script."
 else
   git config --global user.email $GIT_EMAIL
 fi
