35ee686e3ce1d1f144847a387d5027a8e40ddbde
Michaux Kelley <michauxkelley@gmail.com>
2012-07-15 00:36:18 -0400

    Add branch for helper script that manages the git config global e-mail address.

---
+++ global_git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# TODO: make sure GIT_EMAIL are set to something before
+# attempting to run this script.
+if [ $GIT_EMAIL = '' ]; then
+  echo "Please set a global e-mail address for git."
+  echo "Type something like: export GIT_EMAIL='test@example.com'"
+else
+  git config --global user.email $GIT_EMAIL
+fi
